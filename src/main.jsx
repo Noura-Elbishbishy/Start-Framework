2c18fef382d7e7de942b18e1836dc69ab3cc24fd
Noura-Elbishbishy <nouraelbishbishy@gmail.com>
2024-07-28 01:14:57 +0300

    Update base URL configuration

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Start-Framework">  // Ensure this matches your GitHub repo name or Vercel project name
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
